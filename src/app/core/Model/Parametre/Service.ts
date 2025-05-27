d64106d56379f5cb8eff8abe5a6813a727d7f1d2
danielWorj <danielngono002@gmail.com>
2025-05-27 08:21:02 +0100

    add server page

---
+++ src/app/core/Model/Parametre/Service.ts
@@ -0,0 +1,10 @@
+import { CategorieService } from "./CategorieService";
+
+export class Service {
+  idServices ?:number ;
+  intitule !:string ;
+  description !:string ;
+  image !:string;
+  date !:string;
+  categorieService !:CategorieService;
+}
