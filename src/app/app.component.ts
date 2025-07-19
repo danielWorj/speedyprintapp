14f3377fa834405f2082f4e645424e8b39a2eb4f
danielWorj <danielngono002@gmail.com>
2025-07-19 08:13:45 +0100

    dernier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { SidebarComponent } from "./layout/sidebar/sidebar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
